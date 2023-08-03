e65a8cb2ae1b81fa9b53df49a7081f93019a8180
anand nandhu <anandnandhu52@gmail.com>
2023-08-03 12:30:16 +0530

    docker

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 source /parameter-store/params
-curl -L -o /app/rds.amazonaws.com/global/global-bundle.pem https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+curl -L -o /app/rds-combined-ca-bundle.pem https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
 node server.js
